eee9f746d85e06d35991ce367e5d89ecc0f8194b
Besnik Fetahu <bfetahu@gmail.com>
2018-04-07 02:30:32 +0200

    replaced the start and offset with the indexes of paragraphs and sentences which we are analyzing for the citation needed task.

--- preprocessing/cite_needed/src/main/java/Test.java
+++ preprocessing/cite_needed/src/main/java/Test.java
@@ -1,16 +1,94 @@
 import io.FileUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import parser.HTMLWikiParser;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by besnik on 2/27/18.
  */
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        String text = FileUtils.readText("/Users/besnik/Desktop/top_1.html").split("\t")[1];
+    public static HTMLWikiParser wp = new HTMLWikiParser();
 
-        String sub = text.substring(34741  ,36867);
-        System.out.println(sub);
+    public static void main(String[] args) throws IOException {
+        Map<String, String> entity_text = new HashMap<>();
+        BufferedReader reader = FileUtils.getFileReader("/Users/besnik/Documents/L3S/unsourced_statements/html_data/enwiki/wiki_subset.txt");
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String entity = line.substring(0, line.indexOf("\t")).trim();
+            String text = line.substring(line.indexOf("\t")).trim().replace("\\n", "\n");
+            entity_text.put(entity, text);
+        }
+
+        Map<String, Document> entity_doms = new HashMap<>();
+        entity_text.keySet().forEach(e -> entity_doms.put(e, Jsoup.parse(entity_text.get(e))));
+
+        reader = FileUtils.getFileReader("/Users/besnik/Documents/L3S/unsourced_statements/html_data/enwiki/prg_indexed_statements.txt");
+        int line_counter = 0;
+        FileUtils.saveText("", "/Users/besnik/Desktop/log.txt");
+
+        StringBuffer sb = new StringBuffer();
+        while ((line = reader.readLine()) != null) {
+            try {
+                String[] tmp = line.split("\t");
+                if (line_counter == 0) {
+                    line_counter++;
+                    continue;
+                }
+                String entity = tmp[3];
+                String section_id = tmp[4];
+                String section = tmp[5];
+
+                Document doc = entity_doms.get(entity);
+                if (doc == null) {
+                    continue;
+                }
+
+                int prg_idx = Integer.parseInt(tmp[6]);
+                int sentence_idx = Integer.parseInt(tmp[7]);
+
+                String statement = StringEscapeUtils.unescapeHtml4(tmp[8]);
+                String statement_cmp = extractCitations(doc, section_id, section, prg_idx, sentence_idx);
+
+                sb.append(statement).append("\t\t").append(statement_cmp).append("\n");
+
+                if (sb.length() > 10000) {
+                    FileUtils.saveText(sb.toString(), "/Users/besnik/Desktop/log.txt", true);
+                    sb.delete(0, sb.length());
+                }
+            } catch (Exception e) {
+                System.out.println(line);
+                e.printStackTrace();
+            }
+        }
+        FileUtils.saveText(sb.toString(), "/Users/besnik/Desktop/log.txt", true);
     }
+
+
+    public static String extractCitations(Document doc, String section_id, String section_label, int prg_idx, int sentence_idx) {
+        Elements sections = doc.select("section");
+        for (Element section : sections) {
+            String section_id_cmp = section.attr("data-mw-section-id");
+            String section_label_cmp = wp.getSectionName(section, section_id);
+
+            if (section_label.equals(section_label_cmp) && section_id.equals(section_id_cmp)) {
+                Elements paragraphs = section.select("p");
+                if (sentence_idx == -1) {
+                    return paragraphs.get(prg_idx).toString();
+                }
+                String[] prg_text = paragraphs.get(prg_idx).toString().split("\\.\\s+");
+                return prg_text[sentence_idx];
+            }
+        }
+        return null;
+    }
+
 }
--- preprocessing/cite_needed/src/main/java/extraction/HTMLExtractor.java
+++ preprocessing/cite_needed/src/main/java/extraction/HTMLExtractor.java
@@ -19,13 +19,13 @@ import java.util.*;
  * Created by besnik on 2/27/18.
  */
 public class HTMLExtractor {
-    public static boolean remove_citations;
+    public static final int STATEMENT_LENGTH = 200;
 
     public static void main(String[] args) throws IOException {
         String[] args1 = {"-entity_seeds", "/Users/besnik/Documents/L3S/unsourced_statements/all_featured_links.csv",
-                "-out_dir", "/Users/besnik/Documents/L3S/unsourced_statements/html_data/",
-                "-wiki_dump", "/Users/besnik/Documents/L3S/unsourced_statements/html_data/",
-                "-option", "sample", "-lang", "itwiki", "-clean_statements", "true"};
+                "-out_dir", "/Users/besnik/Documents/L3S/unsourced_statements/html_data_all_articles/",
+                "-wiki_dump", "/Users/besnik/Documents/L3S/unsourced_statements/html_data_all_articles/",
+                "-option", "sample", "-lang", "enwiki"};
 
         args = args1;
         String entity_seeds = "", out_dir = "", option = "", wiki_dump = "", lang = "";
@@ -41,8 +41,6 @@ public class HTMLExtractor {
                 wiki_dump = args[++i];
             } else if (args[i].equals("-lang")) {
                 lang = args[++i];
-            } else if (args[i].equals("-clean_statements")) {
-                remove_citations = args[++i].equals("true");
             }
         }
 
@@ -56,7 +54,6 @@ public class HTMLExtractor {
             }
             entities.put(tmp[2].trim(), Integer.parseInt(tmp[0]));
         }
-
         if (option.equals("extract")) {
             extractHTMLWikiPageContent(entities, out_dir, lang);
         } else if (option.equals("statements")) {
@@ -67,7 +64,61 @@ public class HTMLExtractor {
             out_dir += "/" + lang + "/";
             sampleStatements(out_dir);
         }
+
     }
+
+
+    /**
+     * Extract the HTML content of Wikipedia pages. We call the REST API by Wikimedia which returns the HTML content
+     * of an article.
+     *
+     * @param entities
+     * @param out_dir
+     * @param lang
+     * @throws IOException
+     */
+    public static void extractHTMLWikiPageContent(Map<String, Integer> entities, String out_dir, String lang) throws IOException {
+        FileUtils.checkDir(out_dir + "/" + lang + "/");
+        String out_file = out_dir + "/" + lang + "/wiki_subset.txt";
+        FileUtils.saveText("", out_file);
+        lang = lang.replace("wiki", "");
+        for (String entity : entities.keySet()) {
+            String entity_label = URLEncoder.encode(entity);
+            //https://en.wikipedia.org/api/rest_v1/page/html/
+            String url = "https://" + lang + ".wikipedia.org/api/rest_v1/page/html/" + entity_label;
+            String url_body = WebUtils.getURLContent(url).replaceAll("<!--(.*?)-->", "");
+
+            String out_json = entity + "\t" + url_body.replace("\n", "\\n") + "\n";
+            FileUtils.saveText(out_json, out_file, true);
+
+            System.out.printf("Finished extracting HTML content for entity %s.\n", entity);
+        }
+    }
+
+    /**
+     * Store the sampled lines.
+     *
+     * @param lines
+     * @param file
+     * @param out_file
+     * @throws IOException
+     */
+    public static void saveSampledStatements(Set<Integer> lines, String file, String out_file) throws IOException {
+        int line_number = 0;
+        String line;
+        BufferedReader reader = FileUtils.getFileReader(file);
+
+        StringBuffer sb = new StringBuffer();
+        while ((line = reader.readLine()) != null) {
+            if (lines.contains(line_number)) {
+                sb.append(line).append("\n");
+            }
+            line_number++;
+        }
+
+        FileUtils.saveText(sb.toString(), out_file);
+    }
+
 
     public static Set<Integer> sampleLines(String file, int num_samples) throws IOException {
         Random rand = new Random(100);
@@ -105,30 +156,6 @@ public class HTMLExtractor {
     }
 
     /**
-     * Store the sampled lines.
-     *
-     * @param lines
-     * @param file
-     * @param out_file
-     * @throws IOException
-     */
-    public static void saveSampledStatements(Set<Integer> lines, String file, String out_file) throws IOException {
-        int line_number = 0;
-        String line;
-        BufferedReader reader = FileUtils.getFileReader(file);
-
-        StringBuffer sb = new StringBuffer();
-        while ((line = reader.readLine()) != null) {
-            if (lines.contains(line_number)) {
-                sb.append(line).append("\n");
-            }
-            line_number++;
-        }
-
-        FileUtils.saveText(sb.toString(), out_file);
-    }
-
-    /**
      * Sample statements.
      *
      * @param out_dir
@@ -136,13 +163,12 @@ public class HTMLExtractor {
      */
     public static void sampleStatements(String out_dir) throws IOException {
         int num_samples = 1000;
-        Set<Integer> lines_cl = sampleLines(out_dir + "/clean_statements.txt", num_samples);
         Set<Integer> lines = sampleLines(out_dir + "/statements.txt", num_samples);
 
         //store the sampled lines.
         saveSampledStatements(lines, out_dir + "/statements.txt", out_dir + "/sampled_statements.txt");
-        saveSampledStatements(lines_cl, out_dir + "/clean_statements.txt", out_dir + "/sampled_clean_statements.txt");
     }
+
 
     /**
      * Extract the citations and sentences with citations from Wikipedia pages. We output for each sentence
@@ -157,27 +183,28 @@ public class HTMLExtractor {
         lang = lang.replace("wiki", "");
         BufferedReader reader = FileUtils.getFileReader(wiki_file);
         String line;
-        String out_file = !remove_citations ? out_dir + "/statements.txt" : out_dir + "/clean_statements.txt";
+        String out_file = out_dir + "/statements.txt";
 
         HTMLWikiParser wp = new HTMLWikiParser();
-        String header = "entity_id\trevision_id\ttimestamp\tentity_title\tsection\tstart\toffset\tstatement\tparagraph\tcitations\n";
+        String header = "entity_id\trevision_id\ttimestamp\tentity_title\tsection_id\tsection\tprg_idx\tsentence_idx\tstatement\tcitations\n";
         FileUtils.saveText(header, out_file);
 
         //parse the individual entities.
         while ((line = reader.readLine()) != null) {
             String[] data = line.split("\t");
             String entity = data[0].replaceAll(" ", "_");
-            String entity_body_html = data[1];
+            String entity_body_html = data[1].replace("\\n", "\n");
 
             Document entity_doc = Jsoup.parse(entity_body_html);
             Map<String, Map<String, String>> entity_citations = wp.extractCitationFromWikiPage(entity_doc);
             System.out.printf("Entity %s has %d citations.\n", entity, entity_citations.size());
 
-            extractStatementCitations(entity_body_html, entity_doc, entity_citations, out_file, wp, lang);
+            extractStatementCitations(entity_doc, entity_citations, out_file, wp, lang);
         }
     }
 
-    private static void extractStatementCitations(String entity_text, Document doc, Map<String, Map<String, String>> citations, String out_file, HTMLWikiParser wp, String lang) {
+    private static void extractStatementCitations(Document doc, Map<String, Map<String, String>> citations,
+                                                  String out_file, HTMLWikiParser wp, String lang) {
         String page_id = "", revision_id = "", timestamp = "";
         Elements meta_data = doc.select("meta");
         for (Element md : meta_data) {
@@ -203,25 +230,23 @@ public class HTMLExtractor {
             StringBuffer sb = new StringBuffer();
             Elements paragraphs = section.select("p");
 
-            for (Element prg : paragraphs) {
+            for (int prg_id = 0; prg_id < paragraphs.size(); prg_id++) {
+                Element prg = paragraphs.get(prg_id);
                 String prg_text = prg.toString();
-                String paragraph_id = "<p id=\"" + prg.id() + "\"";
-                int paragraph_start = entity_text.indexOf(paragraph_id);
-                if (prg.id().isEmpty() || paragraph_start == -1) {
+                if (prg.id().isEmpty()) {
                     continue;
                 }
 
                 //the paragraph contains citations
                 if (!prg.select("sup").isEmpty()) {
                     //extract sentences and the corresponding citations.
-                    extractSentenceCitations(prg, citations, sb, page_id, revision_id, timestamp, section_name, title, paragraph_start);
-                } else {
-                    int start = paragraph_start;
-                    int offset = start + prg_text.length();
+                    extractSentenceCitations(prg, citations, sb, page_id, revision_id, timestamp, section_id, section_name, title, prg_id);
+                } else if (prg_text.length() > STATEMENT_LENGTH) {
                     prg_text = "<div class=\"unsourced-statement\">" + prg_text.replace("\n", "\\n") + "</div>";
+                    prg_text = StringEscapeUtils.unescapeHtml4(prg_text.replaceAll("\n", "\\n"));
                     sb.append(page_id).append("\t").append(revision_id).append("\t").append(timestamp).append("\t").append(title).append("\t").
-                            append(section_name).append("\t").append(start).append("\t").append(offset).append("\t").
-                            append(StringEscapeUtils.unescapeHtml4(prg_text)).append("\t").append("N/A").append("\t").append("N/A").append("\n");
+                            append(section_id).append("\t").append(section_name).append("\t").append(prg_id).append("\t").append(-1).append("\t").
+                            append(prg_text).append("\t").append("N/A").append("\n");
                 }
             }
 
@@ -240,39 +265,30 @@ public class HTMLExtractor {
      * @param section_name
      * @param title
      */
-    private static void extractSentenceCitations(Element paragraph, Map<String, Map<String, String>> citations, StringBuffer sb,
-                                                 String page_id, String revision_id, String timestamp, String section_name, String title, int paragraph_start) {
-        String prg_raw_text = paragraph.toString();
+    private static void extractSentenceCitations(Element paragraph, Map<String, Map<String, String>> citations,
+                                                 StringBuffer sb, String page_id, String revision_id, String timestamp,
+                                                 String section_id, String section_name, String title, int prg_idx) {
+        String prg_raw_escaped_text = paragraph.toString();
         //split text into sentences.
-        String[] sentences = prg_raw_text.split("\\.\\s+");
+        String[] sentences = prg_raw_escaped_text.split("\\.\\s+");
 
         for (int i = 0; i < sentences.length; i++) {
             String sentence = StringEscapeUtils.unescapeHtml4(sentences[i]);
             //the sentence does not contain any citation
-            if (!sentence.contains("<sup about=") || sentence.length() < 200) {
+            if (!sentence.contains("<sup about=") || sentence.length() < STATEMENT_LENGTH) {
                 continue;
             }
 
             Document sentence_doc = Jsoup.parse(sentences[i]);
             Elements scs = sentence_doc.getElementsByClass("mw-ref");
 
-            //in case we want to remove the citations from the sentence
-            int start = paragraph_start + prg_raw_text.indexOf(sentences[i]);
-            int offset = start + sentence.length();
-            if (sentences.length != 1 && i != sentences.length - 1) {
-                offset += 1;
-                sentence += ".";
-            }
-
-            //this is only for display
-            sentence = remove_citations ? sentence.replaceAll("<sup (.*?)>(.*?)</sup>", "") : sentence;
-
             sb.append(page_id).append("\t").append(revision_id).append("\t").append(timestamp).append("\t").append(title).append("\t").
-                    append(section_name).append("\t").append(start).append("\t").append(offset).append("\t").
-                    append(sentence).append("\t").append(StringEscapeUtils.unescapeHtml4(prg_raw_text)).append("\t");
+                    append(section_id).append("\t").append(section_name).append("\t").append(prg_idx).append("\t").append(i).append("\t").
+                    append("<div class=\"sourced-statement\">").append(sentence.replaceAll("\n", "\\n")).append("</div>").append("\t");
 
             for (Element sc : scs) {
-                sb.append(getCitationAttributes(sc, citations, title));
+                String citation_text = getCitationAttributes(sc, citations, title);
+                sb.append(citation_text);
             }
             sb.append("\n");
         }
@@ -281,7 +297,7 @@ public class HTMLExtractor {
 
     public static String getCitationAttributes(Element in_cite, Map<String, Map<String, String>> citations, String title) {
         StringBuffer sb = new StringBuffer();
-        JSONObject data_mw = new JSONObject(in_cite.attributes().get("data-mw"));
+        JSONObject data_mw = new JSONObject(in_cite.attributes().get("data-mw").toString().replace("\n", "\\n"));
         if (data_mw.has("body") && data_mw.getJSONObject("body").has("id")) {
             String note_ref_cite_id = data_mw.getJSONObject("body").getString("id").replace("mw-reference-text-", "");
 
@@ -302,30 +318,4 @@ public class HTMLExtractor {
         return sb.toString();
     }
 
-    /**
-     * Extract the HTML content of Wikipedia pages. We call the REST API by Wikimedia which returns the HTML content
-     * of an article.
-     *
-     * @param entities
-     * @param out_dir
-     * @param lang
-     * @throws IOException
-     */
-    public static void extractHTMLWikiPageContent(Map<String, Integer> entities, String out_dir, String lang) throws IOException {
-        FileUtils.checkDir(out_dir + "/" + lang + "/");
-        String out_file = out_dir + "/" + lang + "/wiki_subset.txt";
-        FileUtils.saveText("", out_file);
-        lang = lang.replace("wiki", "");
-        for (String entity : entities.keySet()) {
-            String entity_label = URLEncoder.encode(entity);
-            //https://en.wikipedia.org/api/rest_v1/page/html/
-            String url = "https://" + lang + ".wikipedia.org/api/rest_v1/page/html/" + entity_label;
-            String url_body = WebUtils.getURLContent(url).replaceAll("<!--(.*?)-->", "");
-
-            String out_json = entity + "\t" + url_body.replace("\n", "\\n") + "\n";
-            FileUtils.saveText(out_json, out_file, true);
-
-            System.out.printf("Finished extracting HTML content for entity %s.\n", entity);
-        }
-    }
 }
